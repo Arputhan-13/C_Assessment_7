0f89547aa954f1f23806c82b33c0547fb2148b47
Arputhan-13 <karputhan@gmail.com>
2026-06-10 22:59:51 +0530

    Create C_problem_27.c

---
+++ C_problem_27.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+void disp_count_sum14()
+{
+    int a=1, b=0, c, v, m, n, f, j;
+    while(a<100000)
+    {
+    c=a%10;
+    v=(a/10)%10;
+    m=(a/100)%10;
+    n=(a/1000)%10;
+    j=(a/10000)%10;
+    f=c+v+m+n+j;
+    if(f==14)
+    {
+    b=b+1;
+    }
+    a++;
+    }
+    printf("%d\n",b);
+}
+
+int main()
+{
+    disp_count_sum14();
+}
